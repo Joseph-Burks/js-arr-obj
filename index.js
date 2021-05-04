deedc0f62367cf1208fffb83decdf948bc4626a6
Joseph-Burks <joe_burks_3@yahoo.com>
2021-05-04 16:04:16 -0500

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+let array = [
+    1,1,1,1,1,2,2,2,3,3,4,5,6,7,7,7,7,7
+]
+
+let obj = {}
+
+array.forEach(n => {
+    if(obj[n]){
+        obj[n] = obj[n] +1
+    } else {
+        obj[n] = 1
+    }
+})
+
+console.log(array)
+console.log(obj)
